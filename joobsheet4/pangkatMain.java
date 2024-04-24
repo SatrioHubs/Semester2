4e3915ebca3af3bdebff9032bce1d42bd51a90e0
SatrioHubs <satrioramadhani387@gmail.com>
2024-04-24 13:52:39 +0700

    perubahan disebabkan oleh pertanyaan

--- joobsheet4/pangkatMain.java
+++ joobsheet4/pangkatMain.java
@@ -29,24 +29,53 @@ public class pangkatMain {
             png[i].pangkat = pangkat;
             png[i] = new pangkat(nilai, pangkat);
         }
-        
 
-        System.out.println("Hasil - BRUTE FORCE");
-        for (int i = 0; i < element; i++) {
-            System.out.println("HAsil dari "
-            + png[i].nilai + " Pangkat "
-            + png[i].pangkat + " Adalah "
-            + png[i].pangkatBF(png[i].nilai, png[i].pangkat)
-            );
+
+        // System.out.println("Hasil - BRUTE FORCE");
+        // for (int i = 0; i < element; i++) {
+        //     System.out.println("HAsil dari "
+        //     + png[i].nilai + " Pangkat "
+        //     + png[i].pangkat + " Adalah "
+        //     + png[i].pangkatBF(png[i].nilai, png[i].pangkat)
+        //     );
+        // }
+
+        // System.out.println("Hasil - DEVIDE AND CONQUER");
+        // for (int i = 0; i < element; i++) {
+        //     System.out.println("HAsil dari "
+        //     + png[i].nilai + " Pangkat "
+        //     + png[i].pangkat + " Adalah "
+        //     + png[i].pangkatDC(png[i].nilai, png[i].pangkat)
+        //     );
+        // }
+
+        System.out.println("\nPilih Metode Perhitungan Pangkat:");
+        System.out.println("1. Brute Force");
+        System.out.println("2. Divide and Conquer");
+        System.out.print("Masukkan pilihan (1/2): ");
+        int pilihanMetode = sc.nextInt();
+
+        // Memproses pilihan metode perhitungan
+        switch (pilihanMetode) {
+            case 1:
+                System.out.println("\nHasil - BRUTE FORCE");
+                for (int i = 0; i < element; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " Pangkat " + png[i].pangkat + " adalah "
+                            + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            case 2:
+                System.out.println("\nHasil - DIVIDE AND CONQUER");
+                for (int i = 0; i < element; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " Pangkat " + png[i].pangkat + " adalah "
+                            + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+                }
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
+                break;
         }
 
-        System.out.println("Hasil - DEVIDE AND CONQUER");
-        for (int i = 0; i < element; i++) {
-            System.out.println("HAsil dari "
-            + png[i].nilai + " Pangkat "
-            + png[i].pangkat + " Adalah "
-            + png[i].pangkatDC(png[i].nilai, png[i].pangkat)
-            );
-        }
+        sc.close(); // Menutup scanner setelah selesai digunakan
     }
 }
