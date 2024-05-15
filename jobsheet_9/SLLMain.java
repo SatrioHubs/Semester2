f4f49589f5d91ac64a36c340224c94adb95f09b8
SatrioHubs <satrioramadhani387@gmail.com>
2024-05-15 18:46:19 +0700

    first

---
+++ jobsheet_9/SLLMain.java
@@ -0,0 +1,33 @@
+package jobsheet_9;
+
+import joobsheet4.pangkat;
+
+public class SLLMain {
+    public static void main(String[] args) {
+        singleLinkList singLL = new singleLinkList();
+
+        singLL.print();
+        singLL.addFirst(890);
+        singLL.print();
+        singLL.addLast(760);
+        singLL.print();
+        singLL.addFirst(700);
+        singLL.print();
+        singLL.insertAfter(700, 999);
+        singLL.print();
+        singLL.insertAtt(3, 833);
+        singLL.print();
+
+        System.out.println("Data berada pada index ke- " + singLL.getData(1));
+        System.out.println("Data 3 berada pada index ke- " + singLL.indexOf(760));
+
+        singLL.remove(999);
+        singLL.print();
+        singLL.removeAtt(0);
+        singLL.print();
+        singLL.removeFirst();
+        singLL.print();
+        singLL.removeLast();
+        singLL.print();
+    }
+}
